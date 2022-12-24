561f4e982c2cd29e6bd99a3ce1d542a686da4618
jmelm93 <76544513+jmelm93@users.noreply.github.com>
2022-12-23 16:59:44 -0800

    Add files via upload

---
+++ buildAndDeployCorsAnywhere.sh
@@ -0,0 +1,15 @@
+# 
+# Once the deployment is complete, your CORS Anywhere server will be live and available for use. You can access it by using the following URL:
+# https://<your-heroku-app-name>.herokuapp.com/
+# 
+# Simply append the URL of the API you want to make a request to after the base URL of your CORS Anywhere server. 
+# For example, if you want to make a request to the SEMrush API, you would use the following URL:
+# https://YOUR_HEROKU_APP_NAME.herokuapp.com/https://api
+# 
+
+npm install -g heroku
+git clone https://github.com/Rob--W/cors-anywhere.git
+cd cors-anywhere
+heroku create
+git push heroku master
+
